a6ac0e634bf1e39d8b6df22705a9f2d6bee985d8
Lucas Solari <128732015+Lucaso1992@users.noreply.github.com>
2023-05-25 10:04:15 +0000

    cards

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,13 +2,14 @@
 //include images into your bundle
 import React from "react";
 import Navbar from "./Navbar.jsx";
+import Cards from "./Cards.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
 			<Navbar/>
-
+             <Cards title={"Card title"} description={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta numquam ut atque fuga aut laudantium!"} buttonDescription={"Find out more!"}/>
 		</div>
 	);
 };
