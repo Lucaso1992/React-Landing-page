b65e76f919a58cec4a9ba610556e3e6e5435ad28
Lucas Solari <128732015+Lucaso1992@users.noreply.github.com>
2023-05-25 16:13:34 +0000

    jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,16 +4,18 @@ import React from "react";
 import Navbar from "./Navbar.jsx";
 import Cards from "./Cards.jsx";
 import myPosts from "../data/posts.js";
+import Jumbotron from "./Jumbotron.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
 			<Navbar />
+			<Jumbotron />
 			<div className="">
-			{myPosts.map((post) => {
-				return <Cards title={post.title} description={post.description} buttonDescription={post.buttonDescription} key={post.id} />
-			})}
+				{myPosts.map((post) => {
+					return <Cards title={post.title} description={post.description} buttonDescription={post.buttonDescription} key={post.id} />
+				})}
 			</div>
 		</div>
 	);
