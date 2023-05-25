208dfc3ad51f9382df35306c54707533a6a99ba0
Lucas Solari <128732015+Lucaso1992@users.noreply.github.com>
2023-05-25 09:14:14 +0000

    flex navbar

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -5,16 +5,21 @@ const Navbar = () => {
     <div className="container-fluid">
       <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon navbar-dark"></span>
       </button>
-      <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
+      
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    
         <div className="navbar-nav">
           <a className="nav-link active text-light" aria-current="page">Home</a>
           <a className="nav-link text-secondary" >About</a>
           <a className="nav-link text-secondary" >Services</a>
           <a className="nav-link disabled text-secondary">Contact</a>
-        </div>
+        </div> 
+        
+       
       </div>
+    
     </div>
   </nav>)
 }
