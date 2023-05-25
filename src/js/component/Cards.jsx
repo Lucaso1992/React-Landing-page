6ad11572ea9b569a10731230ed7006a060298b48
Lucas Solari <128732015+Lucaso1992@users.noreply.github.com>
2023-05-25 10:08:47 +0000

    cards modificacion

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-export default  function Cards({title, description, buttonDescription}){
+export default function Cards({ title, description, buttonDescription }) {
 
-    return (<div className="card" style={{width: "18rem"}}>
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">{title}</h5>
-      <p className="card-text">{description}</p>
-      <a href="#" className="btn btn-primary">{buttonDescription}</a>
-    </div>
-  </div>
-
+    return (
+        <>
+            <div className="card" style={{ width: "18rem" }}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="#" className="btn btn-primary">{buttonDescription}</a>
+                </div>
+            </div>
+        </>
     );
 };
 
