8731a504643326a2f7ec6cc763af4da830526cf4
Lucas Solari <solarilucas92@gmail.com>
2023-05-31 11:59:45 +0000

    modificaciones

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
 const Jumbotron = () => {
-    return (
-        <div className="card">
-            <div className ="card-body text-bg-secondary p-3">
-                 <h1>A Warm Welcome!</h1>
-                <p className ="card-title">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-                 <a href="#" className="btn btn-primary">Call to action</a>
-            </div>
-        </div>
-    ); 
-}
+  return (
+    <div className="card ">
+      <div className="card-body bg-light p-3">
+        <h1>A Warm Welcome!</h1>
+        <p className="card-title">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat
+        </p>
+        <a href="#" className="btn btn-primary">
+          Call to action!
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Jumbotron; 
+export default Jumbotron;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,18 @@
-
 //include images into your bundle
 import React from "react";
 import Navbar from "./Navbar.jsx";
+import Jumbotron from "./Jumbotron.jsx";
+import Cards from "./Cards.jsx";
 
 //create your first component
 const Home = () => {
-	return (
-
-
-		<div>
-			<Navbar/>
-
-		</div>
-
-	);
+  return (
+    <div>
+      <Navbar />
+      <Jumbotron />
+      <Cards />
+    </div>
+  );
 };
 
 export default Home;
